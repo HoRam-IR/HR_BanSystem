35a61473f4e422745c596b3629cdb52827ab8ed1
Mohammad amin Jafarian <amin.jafarian82@gmail.com>
2021-12-26 07:24:27 +0330

    Support permanet ban
    now expire should be VARCHAR

--- hr_bansystem.sql
+++ hr_bansystem.sql
@@ -15,7 +15,7 @@ CREATE TABLE `hr_bansystem` (
 	`Tokens` LONGTEXT NOT NULL DEFAULT '[]' COLLATE 'utf8mb4_bin',
 	`Reason` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_bin',
 	`isBanned` INT(11) NOT NULL DEFAULT '0',
-	`Expire` INT(11) NOT NULL DEFAULT '0',
+	`Expire` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
 	INDEX `ID` (`ID`) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
